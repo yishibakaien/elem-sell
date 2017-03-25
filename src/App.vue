b797914ca7cf900131d681438da5fa4a02b3190e
yishibakaien <646932161@qq.com>
2017-03-26 00:43:40 +0800

    完成至购物车

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to='/goods'>商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to='/ratings'>评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to='/seller'>商家</router-link>
+      </div>
+    </div>
+    <router-view :seller="seller"></router-view>
+  </div>
+</template>
+
+<script>
+  // 将header 注册到页面 以可以使用 v-header 标签
+  import header from 'components/header/header.vue';
+
+  const ERR_OK = 0;
+
+  export default {
+    data() {
+      return {
+        seller: {}
+      };
+    },
+    /**
+     * created:vue生命周期创建的时候，
+     * vue-rescouce 插件，拓展了$http方法，请求网络数据,
+     * 返回一个属性，需要用 .json() 方法转成JSON
+     * then 第一个参数，成功回调，第二个参数，失败回调
+     */
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+        }
+      }, () => {});
+    },
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import './common/stylus/mixin.styl';
+  .tab
+    display flex
+    width 100%
+    height 40px
+    line-height 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    // border-bottom 1px solid rgba(7, 17, 27, 0.1)
+    .tab-item
+      flex 1
+      text-align center
+      & > a
+        display block
+        font-size 14px
+        color rgb(77, 85 93)
+        &.active
+          color rgb(240, 20, 20)
+</style>
---
+++ src/components/goods/goods.vue
@@ -0,0 +1,245 @@
+<template>
+  <div class="goods">
+    <div class="menu-wrapper" ref="menuWrapper">
+      <ul>
+        <li class="menu-item" v-for="(item, index) in goods" :class="{'current':currentIndex===index}" @click="selectMenu(index, $event)">
+          <span class="text border-1px">
+            <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+      <ul>
+        <li class="food-list" v-for="item in goods" ref="foodList">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li class="food-item border-1px" v-for="food in item.foods">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now"><span class="money">￥</span>{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll';
+  import shopcart from 'components/shopcart/shopcart';
+  const ERR_OK = 0;
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: [],
+        listHeight: [],
+        scrollY: 0
+      };
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          // !height2 当滚动到最底部时，防止超出最大索引值
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.goods = response.data;
+          /**
+           * vue 在更新视图的时候，是异步的，需要在$nextTick()的
+           * 回调中，绑定DOM操作
+           */
+          this.$nextTick(() => {
+            this._initScroll();
+            this._calculateHeight();
+          });
+        }
+      });
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          // 是否启用click事件
+          click: true
+        });
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          click: true,
+          // 1 监听事件的触发时间，1为即时触发，3为延迟到事件完毕后触发
+          // 实时获取滚动的位置
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodList;
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      },
+      selectMenu(index, $event) {
+        // better-scroll 点击事件和 浏览器自带点击事件是不同的
+        // better-scroll 会自定派发一个 点击事件，这个点击事件的
+        // _constructed 为true，浏览器原生点击没有这个事件
+        if (!$event._constructed) {
+          return;
+        }
+        let el = this.$refs.foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+        console.log(index);
+      }
+    },
+    components: {
+      shopcart
+    }
+  };
+</script>
+
+<style lang="stylus">
+  @import '../../common/stylus/mixin.styl';
+  
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+      .menu-item
+        display: table
+        width: 100%
+        height: 54px
+        width: 80px
+        padding: 0 12px
+        line-height: 14px
+        &.current
+          position: reletive
+          margin-top: -1px
+          z-index: 10
+          background: #fff
+          font-weight: 700
+          .text
+            border-none()
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+        .text
+          display: table-cell
+          // width: 56px
+          vertical-align: middle
+          border-1px(rgba(7, 17, 27, 0.1))
+          font-size: 12px
+    .foods-wrapper
+      flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            font-weight: 700
+            white-space: nowrap
+            overflow: hidden
+            text-overflow: ellipsis
+            color: rgb(7, 17, 27)
+          .desc,.extra
+            margin-bottom: 8px
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            margin-bottom: 8px
+            line-height: 12px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              font-weight: 700
+              color: rgb(240, 20, 20)
+              .money
+                font-size: 10px
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              clolor: rgb(143, 153, 159)
+</style>
